35aff2295ae9064584ad46ed8d14ed058b01d9af
Adam Martin <ammartin256@gmail.com>
2020-02-22 17:49:14 -0600

    Testing CI Pipeline

--- src/components/pages/SplashPage.js
+++ src/components/pages/SplashPage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class SplashPage extends Component {
   render() {
-    return <h1>Splash Page</h1>;
+    return <h1>Splash Page Test</h1>;
   }
 }
 
