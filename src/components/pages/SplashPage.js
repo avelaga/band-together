7335b426875bb268238bc10f6216ffffd83412ed
Adam Martin <ammartin256@gmail.com>
2020-02-23 00:00:44 -0600

    Working navbar on master

--- src/components/pages/SplashPage.js
+++ src/components/pages/SplashPage.js
@@ -5,12 +5,7 @@ import '../../App.css';
 export class SplashPage extends Component {
   render() {
     return (
-      <Fragment>
-        <img className="splash-logo" src={Logo} />
-        <div clasName="splash-logo-text-container">
-          <p className="right-side-text">Find the music that you love</p>
-        </div>
-      </Fragment>
+      <h1>Splash Page</h1>
     );
   }
 }
