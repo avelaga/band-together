4eff829ce463f035afd193a501e8a0696978fb6d
Adam Martin <ammartin256@gmail.com>
2020-02-24 09:00:03 -0600

    Fixed sizing of splash-page divs

--- src/components/pages/SplashPage.js
+++ src/components/pages/SplashPage.js
@@ -1,11 +1,20 @@
 import React, { Component, Fragment } from "react";
-import Logo from "../../../dist/images/Logo.png";
-import '../../App.css';
+import Slider from '../layout/Slider';
+import Logo from "../../../dist/images/Logo400.png";
+import './pages.css';
 
 export class SplashPage extends Component {
   render() {
     return (
-      <h1>Splash Page Test</h1>
+      <div className="splash-page" >
+        <div className="splash-logo-container">
+          <img className="splash-logo" src={Logo} />
+        </div>
+        <div className="splash-logo-side-text-container">
+          <p className="splash-logo-side-text">Find the music you love</p>
+        </div>
+        {/* <Slider /> */}
+      </div>
     );
   }
 }
