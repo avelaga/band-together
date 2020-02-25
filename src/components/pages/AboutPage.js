0a00268bb682cfc8daf29b8a749e4f30efb4d51d
Adam Martin <ammartin256@gmail.com>
2020-02-24 23:26:15 -0600

    Added axios call to GitLab stats api

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+const axios = require("axios").default;
 
 export class AboutPage extends Component {
   render() {
-    return (
-        <h1>About Page</h1>
-    );
+    const headers = {
+      headers: {
+        "PRIVATE-TOKEN": "7pfMsQdxg_6z_8PEMssw"
+      }
+    };
+    const commits = axios
+      .get("https://gitlab.com/api/v4/projects/17041074/repository/commits", headers)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    return <h1>About Page</h1>;
   }
 }
 
