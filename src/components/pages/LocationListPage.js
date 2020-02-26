04f4f96556445e160c7ebad3e69c94495d46db8c
Gavin Rodrigue <gavinhrodrigue@yahoo.com>
2020-02-25 22:31:51 -0600

    pictures for the cities

--- src/components/pages/LocationListPage.js
+++ src/components/pages/LocationListPage.js
@@ -1,8 +1,55 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 export class LocationListPage extends Component {
+
+  locationOne = (
+     <Link to = "/locations/:id = 1">
+       <image style = {
+         {width: '150px'}, 
+         {height: '150px'}
+        }
+       src = 'https://cdn.travelpulse.com/images/99999999-9999-9999-9999-999999999999/625b0c80-d52e-33fe-4294-826781c6abec/630x355.jpg'
+       alt = "Austin, Texas"
+       >
+       </image>
+      </Link>
+  );
+
+  locationTwo = (
+     <Link to = "/locations/:id = 2">
+       <image style = {
+         {width: '250px'}, 
+         {height: '150px'}
+        }
+       src = 'https://www.sandiego.org/-/media/images/sdta-site/articles/about-sd/1233x860/sdta-articles-11917-1230x860-0000s-0000-about-sd.jpg?bc=white&h=500&w=700&c=1'
+       alt = "Austin, Texas"
+       >
+       </image>
+      </Link>
+  );
+
+  locationThree = (
+     <Link to = "/locations/:id = 3">
+       <image style = {
+         {width: '150px'}, 
+         {height: '150px'}
+        }
+       src = 'https://media.architecturaldigest.com/photos/5da74823d599ec0008227ea8/16:9/w_2560%2Cc_limit/GettyImages-946087016.jpg'
+       alt = "Austin, Texas"
+       >
+       </image>
+      </Link>
+  );
+
   render() {
-    return <h1>Location List</h1>;
+    return (
+      <div>      
+      {this.locationOne}
+      {this.locationTwo}
+      {this.locationThree}
+      </div>
+    );
   }
 }
 
