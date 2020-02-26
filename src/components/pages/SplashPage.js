0f4eb2ce1b82c0bdc5b6ba6303b9d148c453ed87
Adam Martin <ammartin256@gmail.com>
2020-02-25 23:23:26 -0600

    Working slider

--- src/components/pages/SplashPage.js
+++ src/components/pages/SplashPage.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from "react";
-import Slide from '../layout/Slide';
-import './pages.css';
+import Slide from "../layout/Slide";
+import "./pages.css";
 
 export class SplashPage extends Component {
   render() {
     return (
+      <div className="splash-page">
+        {/* <div className="left-half">
+          <h1>Band Together</h1>
+          <p>Find the music you love.</p>
+          </div> */}
+        {/* <div className="cool-shit right-half"> */}
+        {/* </div> */}
         <Slide />
+      // </div>
     );
   }
 }
