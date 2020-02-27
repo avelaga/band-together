35b58d67145a56b9e5f5ffdc0bcb678ca0c44257
Abhi Velaga <abhinav.velaga@gmail.com>
2020-02-27 17:56:02 -0600

    location cards finished

--- src/components/pages/LocationListPage.js
+++ src/components/pages/LocationListPage.js
@@ -1,47 +1,25 @@
 import React, { Component } from "react";
 import Table from 'react-bootstrap/Table';
-import austin from '../../../dist/images/austin.jpg';
-import chicago from '../../../dist/images/chicago.jpg';
-import losangeles from '../../../dist/images/losangeles.jpeg';
+import AustinImg from '../../../dist/images/austin.jpg';
+import ChicagoImg from '../../../dist/images/chicago.jpg';
+import LAImg from '../../../dist/images/losangeles.jpeg';
+import LocationCard from '../layout/LocationCard.js';
 import "./pages.css";
 
 const concerts = "/concerts"
 const artists = "/artists"
 
 export class LocationListPage extends Component {
-
-  renderTableRow(img, city_url, city, pop, concert_url, artist_url) {
-    return (<tr>
-      <td>
-        <a className="location-table-links " href={city_url}><h2>{city}</h2></a>
-        <img className="location-image" src={img}></img>
-      </td>
-      <td><h4>{pop}</h4>
-        <a className="location-table-links " href={concert_url}>Concerts</a> /
-        <a className="location-table-links " href={artist_url}> Artists</a>
-      </td>
-    </tr>);
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
-      <div className="body">
-        <Table bordered striped hover variant="dark">
-          <thead>
-            <tr>
-              <th><h2>Location</h2></th>
-              <th><h2>Information</h2></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderTableRow(austin, "/locations/1",
-             "Austin, TX", "Population: 950,715 ", concerts, artists)}
-            {this.renderTableRow(chicago, "/locations/1",
-             "Chicago, IL","Population: 2,716,000", concerts, artists)}
-            {this.renderTableRow(losangeles, "/locations/3",
-             "Los Angeles, CA", "Population: 4,000,000", concerts, artists)}
-          </tbody>
-        </Table>
+      <div className="body flex">
+        <LocationCard city={"Austin, TX"} img={AustinImg} city_url={"/locations/1"} pop={"950,715"} concert_url={concerts} artist_url={artists} />
+        <LocationCard city={"Chicago, IL"} img={ChicagoImg} city_url={"/locations/2"} pop={"2,716,000"} concert_url={concerts} artist_url={artists} />
+        <LocationCard city={"Los Angeles, CA"} img={LAImg} city_url={"/locations/3"} pop={"4,000,000"} concert_url={concerts} artist_url={artists} />
       </div>
     );
   }
