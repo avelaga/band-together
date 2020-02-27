b6c7113a45acf2e8dd83d15f24c42262218eeae7
Adam Martin <ammartin256@gmail.com>
2020-02-26 21:53:17 -0600

    Finished splash page

--- src/components/pages/SplashPage.js
+++ src/components/pages/SplashPage.js
@@ -15,8 +15,10 @@ export class SplashPage extends Component {
   render() {
     return (
       <div className="splash-page">
-        <h1 className="splash-text">Band Together</h1>
-        <p className="splash-text">Find the music you love</p>
+        <div className="splash-text-container">
+          <h1 className="splash-text">Band Together</h1>
+          <p className="splash-text">Find the music you love</p>
+        </div>
       </div>
     );
   }
