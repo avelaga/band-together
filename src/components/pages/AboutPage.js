476e81f435e6f31d796cd1abd99297d95ad06208
Abhi Velaga <abhinav.velaga@gmail.com>
2020-02-26 21:28:13 -0600

    issues works

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,4 +1,6 @@
 import React, { Component, useState } from "react";
+import "./pages.css";
+
 const axios = require("axios").default;
 
 export class AboutPage extends Component {
@@ -9,7 +11,14 @@ export class AboutPage extends Component {
     faezah: 0,
     jason: 0,
     gavin: 0,
+    abhiIssues: 0,
+    adamIssues: 0,
+    faezahIssues: 0,
+    gavinIssues: 0,
+    jasonIssues: 0,
   };
+
+  // https://gitlab.com/api/v4/projects/17041074/issues_statistics?assignee_username=avelaga
 
   componentDidMount() {
 
@@ -19,6 +28,7 @@ export class AboutPage extends Component {
       }
     };
 
+    // commit api
     axios
       .get(
         "https://gitlab.com/api/v4/projects/17041074/repository/commits",
@@ -45,44 +55,103 @@ export class AboutPage extends Component {
       .catch(err => {
         console.log(err);
       });
+
+    // issue api
+    axios
+      .get(
+        "https://gitlab.com/api/v4/projects/17041074/issues_statistics?assignee_username=avelaga",
+        headers
+      )
+      .then(res => {
+        this.setState({ abhiIssues: res.data.statistics.counts.closed });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+      axios
+      .get(
+        "https://gitlab.com/api/v4/projects/17041074/issues_statistics?assignee_username=Adam-Bomb",
+        headers
+      )
+      .then(res => {
+        this.setState({ adamIssues: res.data.statistics.counts.closed });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+      axios
+      .get(
+        "https://gitlab.com/api/v4/projects/17041074/issues_statistics?assignee_username=faezahali",
+        headers
+      )
+      .then(res => {
+        this.setState({ faezahIssues: res.data.statistics.counts.closed });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+      axios
+      .get(
+        "https://gitlab.com/api/v4/projects/17041074/issues_statistics?assignee_username=gavinhr",
+        headers
+      )
+      .then(res => {
+        this.setState({ gavinIssues: res.data.statistics.counts.closed });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+      axios
+      .get(
+        "https://gitlab.com/api/v4/projects/17041074/issues_statistics?assignee_username=jason.moy42",
+        headers
+      )
+      .then(res => {
+        this.setState({ jasonIssues: res.data.statistics.counts.closed });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
-    console.log(this.state.commits);
     return (
-      <div>
-        <table >
+      <div className="about-page">
+        <table>
           <tbody>
             <tr>
               <th>Name</th>
               <th>Commits</th>
               <th>Issues</th>
-              <th>Unit Tests</th>
             </tr>
             <tr>
               <td>Adam</td>
               <td>{this.state.commits && this.state.adam}</td>
-              <td>0</td>
+              <td>{this.state.commits && this.state.adamIssues}</td>
             </tr>
             <tr>
               <td>Abhi</td> 
               <td>{this.state.commits && this.state.abhi}</td>
-              <td>0</td>
+              <td>{this.state.commits && this.state.abhiIssues}</td>
             </tr>
             <tr>
               <td>Faezah</td>
               <td>{this.state.commits && this.state.faezah}</td>
-              <td>0</td>
+              <td>{this.state.commits && this.state.faezahIssues}</td>
             </tr>
             <tr>
               <td>Gavin</td>
               <td>{this.state.commits && this.state.gavin}</td>
-              <td>0</td>
+              <td>{this.state.commits && this.state.gavinIssues}</td>
             </tr>
             <tr>
               <td>Jason</td>
               <td>{this.state.commits && this.state.jason}</td>
-              <td>0</td>
+              <td>{this.state.commits && this.state.jasonIssues}</td>
             </tr>
           </tbody>
         </table>
