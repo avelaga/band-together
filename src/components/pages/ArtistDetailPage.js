7097c01e9a459e166e05b6fec25207f5714602bf
Adam Martin <ammartin256@gmail.com>
2020-02-27 08:26:20 -0600

    Started artist detail page, added image and artist name

--- src/components/pages/ArtistDetailPage.js
+++ src/components/pages/ArtistDetailPage.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
+import BillieImg from '../../../dist/images/billie.jpg'
+import "./pages.css";
 
 export class ArtistDetailPage extends Component {
+
+  artists = [
+    {
+      name: "Billie Eilish",
+      genre: "Alternative Rock",
+      upcoming_shows: [],
+      members: ["Billie Eilish"],
+      year_started: 2015,
+      bio: "Test bio"
+
+    }
+  ]
 
   render() {
     const id = this.props.match.params.id;
 
   return (
-    <div>
-      <h1>Artist {id} Detail</h1>
+    <div className="artist-detail-page">
+      <div className="artist-detail">
+        <img src={BillieImg} className="artist-detail-image" />
+        <h1 className="artist-name">{this.artists[id-1].name}</h1>
+      </div>
     </div>
   );
   }
