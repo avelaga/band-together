cc28173886896883af774102db86acd2e4f441df
Gavin Rodrigue <gavinhrodrigue@yahoo.com>
2020-02-27 12:31:08 -0600

    modified the table

--- src/components/pages/LocationListPage.js
+++ src/components/pages/LocationListPage.js
@@ -1,111 +1,53 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import Table from 'react-bootstrap/Table';
 import austin from '../../../dist/images/austin.jpg';
 import chicago from '../../../dist/images/chicago.jpg';
 import losangeles from '../../../dist/images/losangeles.jpeg';
-import sandiego from '../../../dist/images/sandiego.jpg';
-import newyork from '../../../dist/images/newyork.jpg';
 import "./pages.css";
+
+const concerts = "https://bandtogether.events/concerts"
+const artists = "https://bandtogether.events/artists"
 
 export class LocationListPage extends Component {
 
-  locationOne = (
-    <div class="locationCont">
-     <Link to = "/locations/:id = 1">
-       <img style = {
-         {width: '150px'}, 
-         {height: '150px'}
-        }
-       src = {austin}
-       alt = "Austin, Texas"
-       >
-       </img>
-       <div class="bottom-left">Austin, TX</div>
-      </Link>
-      </div>
-  );
+  renderTableRow(location_url, img, city, pop, concert_url, artist_url){
+    return (<tr>
+              <td>
+                <b><h3>{name}</h3></b><img className = "location-image" style={{height:'150px', float: 'right'}} src={img}></img><br></br>
+                <a className="location-table-links " href={location_url}>Location Info</a>&nbsp;&#183;&nbsp;
+              </td>
+              <td><h3>{city}</h3></td>
+              <td><h3>{pop}</h3> 
+              <a className="location-table-links " href={concert_url}>Concerts</a>&nbsp;&#183;&nbsp;
+              <a className="location-table-links " href={artist_url}>Artists</a>&nbsp;&#183;&nbsp;
+              </td>
+            </tr>);
+  }
 
-  locationTwo = (
-    <div class="locationCont">
-     <Link to = "/locations/:id = 2">
-       <img style = {
-         {width: '150px'}, 
-         {height: '150px'}
-        }
-       src = {chicago}
-       alt = "Austin, Texas"
-       >
-       </img>
-       <div class="bottom-left">Chicago, IL</div>
-      </Link>
-    </div>
-  );
-
-  locationThree = (
-    <div class="locationCont">
-     <Link to = "/locations/:id = 3">
-       <img style = {
-         {width: '150px'}, 
-         {height: '150px'}
-        }
-       src = {losangeles}
-       alt = "Los Angeles, CA"
-       >
-       </img>
-       <div class="bottom-left">Los Angeles, CA</div>
-      </Link>
-      </div>
-  );
-
-  locationFour = (
-    <div class="locationCont">
-     <Link to = "/locations/:id = 4">
-       <img style = {
-         {width: '150px'}, 
-         {height: '150px'}
-        }
-       src = {newyork}
-       alt = "New York, NY"
-       >
-       </img>
-       <div class="bottom-left">New York, NY</div>
-      </Link>
-      </div>
-  );
-
-  locationFive = (
-    <div class="locationCont">
-     <Link to = "/locations/:id = 5">
-       <img style = {
-         {width: '150px'}, 
-         {height: '150px'}
-        }
-       src = {sandiego}
-       alt = "San Diego, CA"
-       >
-       </img>
-       <div class="bottom-left">San Diego, CA</div>
-      </Link>
-      </div>
-  );
-
-  render() {
-    return (
-      <div className="body">
-        <table id="location table">
-          <tbody>
-            <tr id="row0">
-              <td id="Austin">{this.locationOne}</td>
-              <td id="Chicago">{this.locationTwo}</td>
-              <td id="Los Angeles">{this.locationThree}</td>
-              <td id="San diego">{this.locationFive}</td>
-              <td id="New York">{this.locationFour}</td>
+  render(){
+    return(  
+      <div className="location-list">
+        <Table bordered striped hover variant="dark">
+          <thead>
+            <tr>
+              <th><h2>Location</h2></th>
+              <th><h2>City</h2></th>
+              <th><h2>Information</h2></th>
             </tr>
+          </thead>
+          <tbody>
+            {this.renderTableRow("https://bandtogether.events/locations/1/",
+             austin, "Austin, TX", "Population: 950,715 ", concerts, artists)}
+            {this.renderTableRow("https://bandtogether.events/locations/2/",
+             chicago, "Chicago, IL", "Population: 2,716,000", concerts, artists)}
+            {this.renderTableRow("https://bandtogether.events/locations/3/",
+             losangeles, "Los Angeles, CA", "Population: 4,000,000", concerts, artists)}
           </tbody>
-       </table>
+        </Table>
       </div>
     );
   }
 }
+
 
 export default LocationListPage
