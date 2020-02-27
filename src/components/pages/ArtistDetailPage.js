4ad9ec3189566d1eecaaeea6ccd071d790b28fff
Adam Martin <ammartin256@gmail.com>
2020-02-27 13:18:42 -0600

    Fixed bad data for artists

--- src/components/pages/ArtistDetailPage.js
+++ src/components/pages/ArtistDetailPage.js
@@ -21,7 +21,7 @@ export class ArtistDetailPage extends Component {
         genre: "Rock",
         upcoming_shows: [],
         members: ["Paul Stanley", "Gene Simmons", "Eric Singer", "Tommy Thayer"],
-        year_started: 2013,
+        year_started: 1973,
         bio:
           "Kiss (often stylized as KIϟϟ) is an American rock band formed in New York City in January 1973 by Paul Stanley, Gene Simmons, Peter Criss, and Ace Frehley. Well known for its members' face paint and stage outfits, the group rose to prominence in the mid-to-late 1970s with their elaborate live performances, which featured fire breathing, blood-spitting, smoking guitars, shooting rockets, levitating drum kits, and pyrotechnics. The band has gone through several lineup changes, with Stanley and Simmons being the only members to feature in every lineup. The original and best-known lineup consisted of Stanley (vocals and rhythm guitar), Simmons (vocals and bass), Frehley (lead guitar and vocals), and Criss (drums and vocals). With their make-up and costumes, they took on the personae of comic book-style characters: the Starchild (Stanley), the Demon (Simmons), the Spaceman or Space Ace (Frehley), and the Catman (Criss). Due to creative differences, both Criss and Frehley had departed the group by 1982.",
         image: KissImg,
@@ -31,7 +31,7 @@ export class ArtistDetailPage extends Component {
           genre: "Pop/Hip hop",
           upcoming_shows: [],
           members: ["Post Malone"],
-          year_started: 1973,
+          year_started: 2013,
           bio:
             "Austin Richard Post (born July 4, 1995), known professionally as Post Malone, is an American rapper, singer, songwriter, and record producer. Known for his introspective songwriting and laconic vocal style, Post has gained acclaim for bending a range of genres including hip hop, pop, country, and rock. He first attained recognition in 2015 following the release of his debut single White Iverson. He subsequently signed a recording contract with Republic Records. Post has sold over 65 million records in the US alone. He received a Diamond certification by the Recording Industry Association of America (RIAA) for Congratulations. His accolades include three American Music Awards, a Billboard Music Award, and an MTV Video Music Award. Additionally, he has received six Grammy Award nominations during his career.",
           image: PostImg,
