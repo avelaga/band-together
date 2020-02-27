7f286b74da3d6ba058e7bc69a73c725d39a96752
Abhi Velaga <abhinav.velaga@gmail.com>
2020-02-26 18:11:55 -0600

    working on saving api data

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,7 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 const axios = require("axios").default;
 
 export class AboutPage extends Component {
+  // state = {
+  //   commitInfo: []
+  // };
+  // constructor(){
+  //   super();
+  // }
   render() {
     const headers = {
       headers: {
@@ -11,12 +17,54 @@ export class AboutPage extends Component {
     const commits = axios
       .get("https://gitlab.com/api/v4/projects/17041074/repository/commits", headers)
       .then(res => {
+
+        // this.setState(({
+        //   commitInfo: [res],
+        // }));
         console.log(res);
+        // console.log(commitInfo);
       })
       .catch(err => {
-        console.log(err);
+        // console.log(err);
       });
-    return <h1>About Page</h1>;
+    return (
+      <div>
+      <h1>{commits}</h1>
+      <table >
+        <tr>
+          <th>Name</th>
+          <th>Commits</th>
+          <th>Issues</th>
+          <th>Unit Tests</th>
+        </tr>
+        <tr>
+          <td>Adam</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>Abhi</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>Faezah</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>Gavin</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>Jason</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+      </table>
+      </div>
+    );
   }
 }
 
