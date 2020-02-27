5a5c2621929f5139636fefbcc5475daa905b2d6f
Gavin Rodrigue <gavinhrodrigue@yahoo.com>
2020-02-27 13:02:51 -0600

    fixed table issues

--- src/components/pages/LocationListPage.js
+++ src/components/pages/LocationListPage.js
@@ -10,14 +10,13 @@ const artists = "https://bandtogether.events/artists"
 
 export class LocationListPage extends Component {
 
-  renderTableRow(location_url, img, city, pop, concert_url, artist_url){
+  renderTableRow(img, city_url, city, pop, concert_url, artist_url){
     return (<tr>
-              <td>
-                <b><h3>{name}</h3></b><img className = "location-image" style={{height:'150px', float: 'right'}} src={img}></img><br></br>
-                <a className="location-table-links " href={location_url}>Location Info</a>&nbsp;&#183;&nbsp;
+              <td width = "300">
+              <img className = "location-image" src={img}></img><br></br>
               </td>
-              <td><h3>{city}</h3></td>
-              <td><h3>{pop}</h3> 
+              <a className="location-table-links " href={city_url}>{city}</a>&nbsp;&#183;&nbsp;
+              <td><h4>{pop}</h4> 
               <a className="location-table-links " href={concert_url}>Concerts</a>&nbsp;&#183;&nbsp;
               <a className="location-table-links " href={artist_url}>Artists</a>&nbsp;&#183;&nbsp;
               </td>
@@ -25,7 +24,7 @@ export class LocationListPage extends Component {
   }
 
   render(){
-    return(  
+    return( 
       <div className="location-list">
         <Table bordered striped hover variant="dark">
           <thead>
@@ -36,12 +35,12 @@ export class LocationListPage extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.renderTableRow("https://bandtogether.events/locations/1/",
-             austin, "Austin, TX", "Population: 950,715 ", concerts, artists)}
-            {this.renderTableRow("https://bandtogether.events/locations/2/",
-             chicago, "Chicago, IL", "Population: 2,716,000", concerts, artists)}
-            {this.renderTableRow("https://bandtogether.events/locations/3/",
-             losangeles, "Los Angeles, CA", "Population: 4,000,000", concerts, artists)}
+            {this.renderTableRow(austin, "https://bandtogether.events/locations/1/",
+             "Austin, TX", "Population: 950,715 ", concerts, artists)}
+            {this.renderTableRow(chicago, "https://bandtogether.events/locations/1/",
+             "Chicago, IL","Population: 2,716,000", concerts, artists)}
+            {this.renderTableRow(losangeles, "https://bandtogether.events/locations/3/",
+             "Los Angeles, CA", "Population: 4,000,000", concerts, artists)}
           </tbody>
         </Table>
       </div>
