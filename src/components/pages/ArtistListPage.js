5e8ef800023465d4bf3986ecfe94688d3f650914
Faezah Ali <faezah@cs.utexas.edu>
2020-02-27 18:25:50 -0600

    fixed formatting, added venue/city

--- src/components/pages/ArtistListPage.js
+++ src/components/pages/ArtistListPage.js
@@ -2,24 +2,29 @@ import React, { Component, Children } from "react";
 import Table from 'react-bootstrap/Table';
 import PostImg from '../../../dist/images/post_malone.png';
 import BillieImg from '../../../dist/images/billie_eilish.jpg';
-import KISSImg from '../../../dist/images/kiss.jpg';
+import KissImg from '../../../dist/images/kiss.jpg';
+
 
 export class ArtistListPage extends Component { 
-  renderTableRow(img, city_url, city, pop, concert_url, location_url){
+
+  concerts = "/concerts"
+  locations = "/locations"
+  renderTableRow(img, artist, artist_url, genre, location, location_url, concert, concert_url){
     return (<tr>
-              <td width = "300">
-              <img className = "artist-image" src={img}></img><br></br>
-              </td>
-              <a className="location-table-links " href={city_url}><h5>{city}</h5></a>
-              <td><h4>{pop}</h4> 
-              <a className="location-table-links " href={concert_url}>Concerts</a>;
-              <a className="location-table-links " href={location_url}>Locations</a>;
+              <td width = "150">
+              <img className = "artist-image" width = "150px" src={img}></img><br></br>
+              <a className="artist-table-links " href={artist_url}><h5>{artist}</h5></a>
+              </td> 
+              <td>{genre}</td>
+              <td> 
+              <a className="artist-table-links " href={location_url}>{location}</a>  <t></t>
+              <a className="artist-table-links " href={concert_url}>{concert}</a>
+
               </td>
             </tr>);
   }
-}
 
-render();{
+render() {
   return( 
     <div className="body">
       <Table bordered striped hover variant="dark">
@@ -27,47 +32,24 @@ render();{
           <tr>
             <th><h2>Artist</h2></th>
             <th><h2>Genre</h2></th>
-            <th><h2>Touring City</h2></th>
+            <th><h2>Tour Venue and City</h2></th>
           </tr>
         </thead>
         <tbody>
-          {this.renderTableRow(PostMalone, "https://bandtogether.events/artists/1/", "Hip-Hop", concerts, locations)}
-          {this.renderTableRow(BillieEilish, "https://bandtogether.events/artists/2/", "Alternative-Pop", concerts, locations)}
-          {this.renderTableRow(Kiss, "https://bandtogether.events/artists/3/", "Hard Rock", concerts, locations)}
+          {this.renderTableRow(BillieImg, "Billie Eilish" ,"https://bandtogether.events/artists/1/", "Alternative Pop", 
+                                          "Frank C. Erwin, Jr. Special Events Center", "https://bandtogether.events/concerts/1/" ,
+                                          "Los Angeles, CA", "https://bandtogether.events/locations/3/")}
+          {this.renderTableRow(KissImg, "Kiss" ,"https://bandtogether.events/artists/2/",  "Hard Rock",
+                                        "United Center", "https://bandtogether.events/concerts/2/", 
+                                        "Chicago, IL", "https://bandtogether.events/locations/2/")}
+          {this.renderTableRow(PostImg, "Post Malone" ,"https://bandtogether.events/artists/3/", "Hip Hop",
+                                        "STAPLES Center","https://bandtogether.events/concerts/3/",
+                                        "Austin, TX", "https://bandtogether.events/locations/1/")}
         </tbody>
       </Table>
     </div>
   );
 }
-
-export default ArtistListPage;
-
-
-/*
-  render(){
-    return( 
-      <div className="body">
-        <Table bordered striped hover variant="dark">
-          <thead>
-            <tr>
-              <th><h2>Location</h2></th>
-              <th><h2>City</h2></th>
-              <th><h2>Information</h2></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderTableRow(austin, "https://bandtogether.events/locations/1/",
-             "Austin, TX", "Population: 950,715 ", concerts, artists)}
-            {this.renderTableRow(chicago, "https://bandtogether.events/locations/1/",
-             "Chicago, IL","Population: 2,716,000", concerts, artists)}
-            {this.renderTableRow(losangeles, "https://bandtogether.events/locations/3/",
-             "Los Angeles, CA", "Population: 4,000,000", concerts, artists)}
-          </tbody>
-        </Table>
-      </div>
-    );
-  }
 }
 
-export default ConcertListPage;
- */
+export default ArtistListPage;
