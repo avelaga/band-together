4a141ace299e5c4228fe9cba28e403b82fa634ab
Abhi Velaga <abhinav.velaga@gmail.com>
2020-03-28 23:10:01 -0500

    broken :(

--- src/App.js
+++ src/App.js
@@ -37,17 +37,18 @@ const errorGif = {
 function App() {
 
   return (
-    <Switch>
-      <Route exact path='/' exact component={SplashPage} />
-      <Route exact path='/about' exact component={AboutPage} />
-      <Route exact path='/locations' exact component={LocationListPage} />
-      <Route exact path='/locations/:id' exact component={LocationDetailPage} />
-      <Route exact path='/artists' exact component={ArtistListPage} />
-      <Route exact path='/artists/:id' exact component={ArtistDetailPage} />
-      <Route exact path='/concerts' exact component={ConcertListPage} />
-      <Route exact path='/concerts/:id' exact component={ConcertDetailPage} />
-      <Route component={ErrorPage} />
-    </Switch>
+    // <Switch>
+    //   <Route exact path='/' exact component={SplashPage} />
+    //   <Route exact path='/about' exact component={AboutPage} />
+    //   <Route exact path='/locations' exact component={LocationListPage} />
+    //   <Route exact path='/locations/:id' exact component={LocationDetailPage} />
+    //   <Route exact path='/artists' exact component={ArtistListPage} />
+    //   <Route exact path='/artists/:id' exact component={ArtistDetailPage} />
+    //   <Route exact path='/concerts' exact component={ConcertListPage} />
+    //   <Route exact path='/concerts/:id' exact component={ConcertDetailPage} />
+    //   <Route component={ErrorPage} />
+    // </Switch>
+    <h1>Hello World</h1>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import { expect } from "chai";
+var jsdom = require("mocha-jsdom");
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+global.document = jsdom({
+  url: "http://localhost:8080/"
+});
+
+import App from "./App";
+
+let rootContainer;
+
+beforeEach(() => {
+  rootContainer = document.createElement("div");
+  document.body.appendChild(rootContainer);
+});
+
+afterEach(() => {
+  document.body.removeChild(rootContainer);
+  rootContainer = null;
+});
+
+describe("App Component Testing", () => {
+  it("Renders Hello World Title", () => {
+    act(() => {
+      ReactDOM.render(<App />, rootContainer);
+    });
+    const h1 = rootContainer.querySelector("h1");
+    expect(h1.textContent).to.equal("Hello World");
+  });
 });
