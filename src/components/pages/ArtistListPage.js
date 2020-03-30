0f2a1447902b755c2f1aadc7c2d9f64d4410e920
Abhi Velaga <abhinav.velaga@gmail.com>
2020-03-30 15:50:07 -0500

    WORKING PAGINATION, fixed vertical scroll bar

--- src/components/pages/ArtistListPage.js
+++ src/components/pages/ArtistListPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ArtistCard from '../layout/ArtistCard.js';
+import { List, Pagination } from "semantic-ui-react";
+// import "semantic-ui-css/semantic.min.css";
 const axios = require("axios").default;
 
 export class ArtistListPage extends Component {
@@ -9,7 +11,8 @@ export class ArtistListPage extends Component {
       count: null,
       next: null,
       prev: null,
-      results: null
+      results: null,
+      page: 1
     }
   }
 
@@ -32,13 +35,42 @@ export class ArtistListPage extends Component {
       });
   }
 
+  setPageNum = (event, { activePage }) => {
+    this.setState({ page: activePage });
+    let url = "http://192.168.1.170:8000/restapi/artist";
+    axios
+      .get(
+        url + "?page=" + activePage
+      )
+      .then(res => {
+        this.setState({
+          count: res.data.count,
+          next: res.data.next,
+          prev: res.data.previous,
+          results: res.data.results
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
+    console.log(this.state.page);
     return (
       <div className="body">
         <div className="flex">
-        {this.state.results && this.state.results.map((value, index)=> {
-          return <ArtistCard key={index} name={value.name} genre={value.genre} img={value.image} artist_url={"artists/1"} concert_url={"concerts/1"} city_url={"locations/1"} spotify_url={value.spotify_url} twitter_url={value.twitter_url} wiki_url={value.wiki_url} website={value.website} />
-        })}
+          {this.state.results && this.state.results.map((value, index) => {
+            return <ArtistCard key={index} name={value.name} genre={value.genre} img={value.image} artist_url={"artists/1"} concert_url={"concerts/1"} city_url={"locations/1"} spotify_url={value.spotify_url} twitter_url={value.twitter_url} wiki_url={value.wiki_url} website={value.website} />
+          })}
+        </div>
+        <div className="pagination-menu">
+          <Pagination
+            activePage={this.state.page}
+            totalPages={Math.ceil(this.state.count / 10)}
+            siblingRange={1}
+            onPageChange={this.setPageNum}
+          />
         </div>
       </div>
     );
