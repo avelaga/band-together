c25429a7296bf2b4da6653289ef6146c93481551
Abhi Velaga <abhinav.velaga@gmail.com>
2020-03-31 16:31:04 -0500

    added loading spinner

--- src/components/pages/ConcertListPage.js
+++ src/components/pages/ConcertListPage.js
@@ -57,19 +57,24 @@ export class ConcertListPage extends Component {
   render() {
     return (
       <div className="body">
-        <div className="flex">
-          {this.state.results && this.state.results.map((value, index) => {
-            return <ConcertCard key={index} name={value.artistName} img={value.venueImage} city={value.locationName} date={value.date} time={value.time} ticket_min={value.ticket_min} ticket_max={value.ticket_max} location_url={"locations/" + value.id} artist_url={"artists/" + value.artistId} concert_url={"concerts/" + value.id} />
-          })}
-        </div>
-        <div className="pagination-menu">
-          <Pagination
-            activePage={this.state.page}
-            totalPages={Math.ceil(this.state.count / 10)}
-            siblingRange={1}
-            onPageChange={this.setPageNum}
-          />
-        </div>
+        {!this.state.results && <div className="lds-ripple"><div></div><div></div></div>}
+        {this.state.results &&
+          <div>
+            <div className="flex">
+              {this.state.results.map((value, index) => {
+                return <ConcertCard key={index} name={value.artistName} img={value.venueImage} city={value.locationName} date={value.date} time={value.time} ticket_min={value.ticket_min} ticket_max={value.ticket_max} location_url={"locations/" + value.id} artist_url={"artists/" + value.artistId} concert_url={"concerts/" + value.id} />
+              })}
+            </div>
+            <div className="pagination-menu">
+              <Pagination
+                activePage={this.state.page}
+                totalPages={Math.ceil(this.state.count / 10)}
+                siblingRange={1}
+                onPageChange={this.setPageNum}
+              />
+            </div>
+          </div>
+        }
       </div>
     );
   }
