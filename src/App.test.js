a99da632f2c5bf497b228e55b208293cccec7714
faezahali <amreen225@gmail.com>
2020-03-31 14:41:09 -0500

    tweaked formatting

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
+/*npm i mocha
+npm i -D chai
+npm install --save-dev ignore-styles
+npm i --save-dev mocha-jsdom */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
 import { expect , should} from "chai";
 var jsdom = require("mocha-jsdom");
-
 
 
 var chai = require('chai');
@@ -35,62 +39,61 @@ afterEach(() => {
 describe("Load Pages", function()  {
 
     it("should load splash page without error", function()  {
-      chai.request('http://localhost:8080/').get('/')
+      chai.request('http://localhost:8080').get('/')
       .end(function(err, res){
         //expect(res).to.have.status(200);
         res.should.have.status(200);
-        if 
         //done();
       });
       });
 
     it("should load about page without error", function()  {
-      chai.request('http://localhost:8080/').get('/about')
+      chai.request('http://localhost:8080').get('/about')
       .end(function(res){
         res.should.have.status(200);
       });
     });
 
     it("should load location list page without error", function()  {
-      chai.request('http://localhost:8080/').get('/locations')
+      chai.request('http://localhost:8080').get('/locations')
       .end(function(res){
         res.should.have.status(200);
       });
     });
 
     it("should load location detail page without error", function()  {
-      chai.request('http://localhost:8080/').get('/locations/:id')
+      chai.request('http://localhost:8080').get('/locations/:id')
       .end(function(res){
         res.should.have.status(200);
       });
     });
 
     it("should load artists list page without error", function()  {
-      chai.request('http://localhost:8080/').get('/artists')
+      chai.request('http://localhost:8080').get('/artists')
       .end(function(res){
         res.should.have.status(200);
       });
     });
 
     it("should load artists detail page without error", function()  {
-      chai.request('http://localhost:8080/').get('/artists/:id')
+      chai.request('http://localhost:8080').get('/artists/:id')
       .end(function(res){
         res.should.have.status(200);
       });
     });
 
     it("should load concert list page without error", function()  {
-      chai.request('http://localhost:8080/').get('/concerts')
+      chai.request('http://localhost:8080').get('/concerts')
       .end(function(res){
         res.should.have.status(200);
       });
     });
 
     it("should load concerts detail page without error", function()  {
-      chai.request('http://localhost:8080/').get('/concerts/:id')
+      chai.request('http://localhost:8080').get('/concerts/:id')
       .end(function(res){
         res.should.have.status(200);
-        done();
+        //done();
       });
     });
 
@@ -107,9 +110,3 @@ describe("Load Pages", function()  {
 
 //});
 
-
-/* <Route exact path='/locations/:id' exact component={LocationDetailPage} />
-      <Route exact path='/artists' exact component={ArtistListPage} />
-      <Route exact path='/artists/:id' exact component={ArtistDetailPage} />
-      <Route exact path='/concerts' exact component={ConcertListPage} />
-      <Route exact path='/concerts/:id' ex */
