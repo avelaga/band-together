33a941f0109be6d22a384c72e511410010b0326b
Abhi Velaga <abhinav.velaga@gmail.com>
2020-03-31 13:46:09 -0500

    switched backend url to production aws server

--- src/components/pages/ArtistListPage.js
+++ src/components/pages/ArtistListPage.js
@@ -16,7 +16,7 @@ export class ArtistListPage extends Component {
   }
 
   componentDidMount() {
-    let url = "http://192.168.1.170:8000/restapi/artist";
+    let url = "http://bandtogetherapi.xyz/restapi/artist";
     axios
       .get(
         url
@@ -36,7 +36,7 @@ export class ArtistListPage extends Component {
 
   setPageNum = (event, { activePage }) => {
     this.setState({ page: activePage });
-    let url = "http://192.168.1.170:8000/restapi/artist";
+    let url = "http://bandtogetherapi.xyz/restapi/artist";
     axios
       .get(
         url + "?page=" + activePage
