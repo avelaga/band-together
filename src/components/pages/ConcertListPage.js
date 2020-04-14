a6a8688c91ce2fa4341a7abcb0f06a4dd1bff205
Abhi Velaga <abhinav.velaga@gmail.com>
2020-04-13 22:32:12 -0500

    added search to concert list

--- src/components/pages/ConcertListPage.js
+++ src/components/pages/ConcertListPage.js
@@ -1,6 +1,7 @@
 import React, { Component, Children } from "react";
 import ConcertCard from '../layout/ConcertCard.js';
 import { Pagination } from "semantic-ui-react";
+import SearchField from "react-search-field";
 const axios = require("axios").default;
 
 export class ConcertListPage extends Component {
@@ -11,8 +12,33 @@ export class ConcertListPage extends Component {
       next: null,
       prev: null,
       results: null,
-      page: 1
+      page: 1,
+      searchTerm: ''
     }
+  }
+
+  newSearch(value) {
+    this.setState({
+      searchTerm: "query=" + value
+    });
+
+    let url = "http://192.168.1.170:8000/restapi/concert/search?" + "query=" + value;
+    axios
+      .get(
+        url
+      )
+      .then(res => {
+        this.setState({
+          count: res.data.count,
+          next: res.data.next,
+          prev: res.data.previous,
+          results: res.data.results,
+          page: 1
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   componentDidMount() {
@@ -58,8 +84,15 @@ export class ConcertListPage extends Component {
     return (
       <div className="body">
         {!this.state.results && <div className="lds-ripple"><div></div><div></div></div>}
-        {this.state.results &&
+        {(this.state.results && this.state.count > 0) &&
           <div>
+            <div className="search-div">
+              <SearchField
+                placeholder="Search..."
+                onEnter={(e) => { this.newSearch(e) }}
+                onSearchClick={(e) => { this.newSearch(e) }}
+              />
+            </div>
             <div className="flex">
               {this.state.results.map((value, index) => {
                 return <ConcertCard key={index} name={value.artistName} img={value.venueImage ? value.venueImage : value.artistImage} city={value.locationName} date={value.date} time={value.time} ticket_min={value.ticket_min} ticket_max={value.ticket_max} location_url={"locations/" + value.id} artist_url={"artists/" + value.artistId} concert_url={"concerts/" + value.id} />
@@ -75,9 +108,29 @@ export class ConcertListPage extends Component {
             </div>
           </div>
         }
+
+        {/* If count = 0, show no results page */}
+        {(this.state.count === 0) &&
+          <div>
+            <div className="search-div">
+              <SearchField
+                placeholder="Search..."
+                onEnter={(e) => { this.newSearch(e) }}
+                onSearchClick={(e) => { this.newSearch(e) }}
+              />
+            </div>
+            <div className="flex" style={noResults}>
+              <h1>No results found</h1>
+            </div>
+          </div>
+        }
       </div>
     );
   }
 }
 
 export default ConcertListPage;
+
+const noResults = {
+  color: 'white'
+}
