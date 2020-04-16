a8566009aa94d106eb2c62ae2bd72a57759f1587
Abhi Velaga <abhinav.velaga@gmail.com>
2020-04-16 13:53:40 -0500

    artist sort + follower and popularity filter work

--- src/components/pages/ArtistListPage.js
+++ src/components/pages/ArtistListPage.js
@@ -15,22 +15,68 @@ export class ArtistListPage extends Component {
       prev: null,
       results: null,
       page: 1,
-      searchTerm: '',
-      popularitySliders: [0, 100],
-      followerSliders: [0, 100],
-      searched: false
+      searched: false,
+      query: '',
+      sortBy: "name",
+      ascending: 1,
+      minFollowers: 0,
+      maxFollowers: 34000000,
+      minPop: 0,
+      maxPop: 100,
+      genre: '',
+      default: {
+        count: null,
+        next: null,
+        prev: null,
+        results: null,
+        page: 1,
+        searched: false,
+        query: '',
+        sortBy: "name",
+        ascending: 1,
+        minFollowers: 0,
+        maxFollowers: 34000000,
+        minPop: 0,
+        maxPop: 100,
+        genre: ''
+      }
     }
   }
 
   newSearch(value) {
-    this.setState({
-      searchTerm: "query=" + value
-    });
+    console.log(value);
+    if (value === "") {
+      console.log("it was null");
+      this.setState({
+        query: value,
+        searched: false
+      }, this.updateState);
+    } else {
+      this.setState({
+        query: value,
+        searched: true
+      }, this.updateState);
+    }
+  }
 
-    let url = "http://192.168.1.170:8000/restapi/artist/search?" + "query=" + value;
+  updateState() {
+    let url = "http://192.168.1.170:8000/restapi/artist";
+    let options = {
+      params: {
+        page: this.state.page,
+        query: this.state.query,
+        sortBy: this.state.sortBy,
+        ascending: this.state.ascending,
+        minFollowers: this.state.minFollowers,
+        maxFollowers: this.state.maxFollowers,
+        minPop: this.state.minPop,
+        maxPop: this.state.maxPop,
+        genre: this.state.genre,
+      }
+    };
     axios
       .get(
-        url
+        url, options
       )
       .then(res => {
         this.setState({
@@ -38,8 +84,6 @@ export class ArtistListPage extends Component {
           next: res.data.next,
           prev: res.data.previous,
           results: res.data.results,
-          page: 1,
-          searched: true
         });
       })
       .catch(err => {
@@ -47,68 +91,69 @@ export class ArtistListPage extends Component {
       });
   }
 
-
   componentDidMount() {
-    let url = "http://192.168.1.170:8000/restapi/artist";
-    axios
-      .get(
-        url
-      )
-      .then(res => {
-        this.setState({
-          count: res.data.count,
-          next: res.data.next,
-          prev: res.data.previous,
-          results: res.data.results
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.setState({ page: 1 }, this.updateState);
   }
 
   setPageNum = (event, { activePage }) => {
-    this.setState({ page: activePage });
-    let url = "http://192.168.1.170:8000/restapi/artist/search?";
-    axios
-      .get(
-        url + "page=" + activePage + "&" + this.state.searchTerm
-      )
-      .then(res => {
-        this.setState({
-          count: res.data.count,
-          next: res.data.next,
-          prev: res.data.previous,
-          results: res.data.results
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.setState({ page: activePage }, this.updateState);
   };
 
-  popularitySlidersChange = (event, newValue) => {
+  sortName = (event) => {
+    this.setState({ sortBy: "name" }, this.updateState);
+  }
 
-    // ADAM: popularity slider changes comes here. newValue is int[] of size 2 with format of [min, max]
+  sortGenre = (event) => {
+    this.setState({ sortBy: "genre" }, this.updateState);
+  }
+
+  sortFollowers = (event) => {
+    this.setState({ sortBy: "num_spotify_followers" }, this.updateState);
+  }
+
+  sortPopularity = (event) => {
+    this.setState({ sortBy: "popularity_score" }, this.updateState);
+  }
+
+  sortAscending = (event) => {
+    this.setState({ ascending: 1 }, this.updateState);
+  }
+
+  sortDescending = (event) => {
+    this.setState({ ascending: -1 }, this.updateState);
+  }
+
+  popularitySlidersChange = (event, newValue) => {
     this.setState({
-      popularitySliders: newValue
-    });
+      minPop: newValue[0],
+      maxPop: newValue[1]
+    }, this.updateState);
   };
 
   followerSlidersChange = (event, newValue) => {
-    
-    // ADAM: follower slider changes comes here. newValue is int[] of size 2 with format of [min, max]
     this.setState({
-      followerSliders: newValue
-    });
+      minFollowers: newValue[0],
+      maxFollowers: newValue[1]
+    }, this.updateState);
   };
 
-  sortGenre = (event) => {
-    console.log("SORT BY GENRE");
-  }
-
-  sortName = (event) => {
-    console.log("SORT BY NAME");
+  reset = (event) => {
+    this.setState({
+      count: null,
+      next: null,
+      prev: null,
+      results: null,
+      page: 1,
+      searched: false,
+      query: '',
+      sortBy: "name",
+      ascending: 1,
+      minFollowers: 0,
+      maxFollowers: 34000000,
+      minPop: 0,
+      maxPop: 100,
+      genre: ''
+    }, this.updateState);
   }
 
   render() {
@@ -117,18 +162,24 @@ export class ArtistListPage extends Component {
         <div className="appear-second">
           <h1 className="title">Artists</h1>
           {!this.state.results && <div className="lds-ripple"><div></div><div></div></div>}
-          {(this.state.results && this.state.count > 0) &&
+          {this.state.results &&
 
             <div>
               <div className="search-div flex">
-
-                <div style={buttonStyle} onClick={this.sortGenre}>Genre</div>
+                <div className="title">sort by: </div>
                 <div style={buttonStyle} onClick={this.sortName}>Name</div>
+                <div style={buttonStyle} onClick={this.sortGenre}>Genre</div>
+                <div style={buttonStyle} onClick={this.sortFollowers}>Followers</div>
+                <div style={buttonStyle} onClick={this.sortPopularity}>Populairty</div>
+                <div style={buttonStyle} onClick={this.sortAscending}>Ascending</div>
+                <div style={buttonStyle} onClick={this.sortDescending}>Descending</div>
+
+                <div className="title">filter by: </div>
 
                 <div style={sliderDiv}>
                   <h6>Popularity</h6>
                   <Slider
-                    value={this.state.popularitySliders}
+                    value={[this.state.minPop, this.state.maxPop]}
                     onChange={this.popularitySlidersChange}
                     valueLabelDisplay="auto"
                     aria-labelledby="range-slider"
@@ -137,13 +188,16 @@ export class ArtistListPage extends Component {
                 </div>
 
                 <div style={sliderDiv}>
-                  <h6>Popularity</h6>
+                  <h6>Followers</h6>
                   <Slider
-                    value={this.state.followerSliders}
+                    value={[this.state.minFollowers, this.state.maxFollowers]}
                     onChange={this.followerSlidersChange}
                     valueLabelDisplay="auto"
                     aria-labelledby="range-slider"
                     style={sliderStyle}
+
+                    min={0}
+                    max={34000000}
                   />
                 </div>
 
@@ -153,53 +207,48 @@ export class ArtistListPage extends Component {
                   onSearchClick={(e) => { this.newSearch(e) }}
                 />
 
-              </div>
-              <div className="flex">
-                {this.state.results.map((value, index) => {
-                  return <ArtistCard key={index} name={value.name} genre={value.genre} img={value.image} artist_url={"artists/" + value.id} spotify_url={value.spotify_url} twitter_url={value.twitter_url} wiki_url={value.wiki_url} website={value.website} followers={value.num_spotify_followers} popularity={value.popularity_score} search={this.state.searched}/>
-                })}
-              </div>
-              <div className="pagination-menu">
+                <div style={buttonStyle} onClick={this.reset}>RESET</div>
 
-                {/* desktop */}
-                <MediaQuery minDeviceWidth={500}>
-                  <Pagination
-                    activePage={this.state.page}
-                    totalPages={Math.ceil(this.state.count / 10)}
-                    siblingRange={1}
-                    onPageChange={this.setPageNum}
-
-                  />
-                </MediaQuery>
-
-                {/* mobile */}
-                <MediaQuery maxDeviceWidth={500}>
-                  <Pagination
-                    activePage={this.state.page}
-                    totalPages={Math.ceil(this.state.count / 10)}
-                    siblingRange={1}
-                    onPageChange={this.setPageNum}
-                    ellipsisItem={null}
-                    boundaryRange={0}
-                  />
-                </MediaQuery>
               </div>
-            </div>
-          }
+              {/* If count = 0, show no results page */}
+              {(this.state.count === 0) &&
+                <div className="flex" style={white}>
+                  <h1>No results found</h1>
+                </div>
+              }
+              {/* If count > 0, show results + pagination menu */}
+              {(this.state.count > 0) &&
+                <div>
+                  <div className="flex">
+                    {this.state.results.map((value, index) => {
+                      return <ArtistCard key={index} name={value.name} genre={value.genre} img={value.image} artist_url={"artists/" + value.id} spotify_url={value.spotify_url} twitter_url={value.twitter_url} wiki_url={value.wiki_url} website={value.website} followers={value.num_spotify_followers} popularity={value.popularity_score} search={this.state.searched} />
+                    })}
+                  </div>
+                  <div className="pagination-menu">
+                    {/* desktop */}
+                    <MediaQuery minDeviceWidth={500}>
+                      <Pagination
+                        activePage={this.state.page}
+                        totalPages={Math.ceil(this.state.count / 10)}
+                        siblingRange={1}
+                        onPageChange={this.setPageNum}
 
-          {/* If count = 0, show no results page */}
-          {(this.state.count === 0) &&
-            <div>
-              <div className="search-div">
-                <SearchField
-                  placeholder="Search..."
-                  onEnter={(e) => { this.newSearch(e) }}
-                  onSearchClick={(e) => { this.newSearch(e) }}
-                />
-              </div>
-              <div className="flex" style={white}>
-                <h1>No results found</h1>
-              </div>
+                      />
+                    </MediaQuery>
+                    {/* mobile */}
+                    <MediaQuery maxDeviceWidth={500}>
+                      <Pagination
+                        activePage={this.state.page}
+                        totalPages={Math.ceil(this.state.count / 10)}
+                        siblingRange={1}
+                        onPageChange={this.setPageNum}
+                        ellipsisItem={null}
+                        boundaryRange={0}
+                      />
+                    </MediaQuery>
+                  </div>
+                </div>
+              }
             </div>
           }
         </div>
@@ -217,7 +266,6 @@ const white = {
 
 const buttonStyle = {
   backgroundColor: 'rgb(0, 119, 255)',
-  // width: '300px',
   borderRadius: '5px',
   padding: '7px',
   marginRight: '5px',
