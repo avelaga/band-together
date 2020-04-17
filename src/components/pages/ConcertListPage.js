a4e8f926a6b6554ae922723a094510344cc58844
Abhi Velaga <abhinav.velaga@gmail.com>
2020-04-16 21:29:26 -0500

    added region to concert card

--- src/components/pages/ConcertListPage.js
+++ src/components/pages/ConcertListPage.js
@@ -214,7 +214,7 @@ export class ConcertListPage extends Component {
                 <div>
                   <div className="flex">
                     {this.state.results.map((value, index) => {
-                      return <ConcertCard key={index} name={value.artistName} img={value.venueImage ? value.venueImage : value.artistImage} city={value.locationName} date={value.date} time={value.time} ticket_min={value.ticket_min} ticket_max={value.ticket_max} location_url={"locations/" + value.id} artist_url={"artists/" + value.artistId} concert_url={"concerts/" + value.id} venueName={value.venueName} artistGenre={value.artistGenre} searched={this.state.searched} query = {this.state.query} />
+                      return <ConcertCard key={index} name={value.artistName} img={value.venueImage ? value.venueImage : value.artistImage} city={value.locationName} region={value.region} date={value.date} time={value.time} ticket_min={value.ticket_min} ticket_max={value.ticket_max} location_url={"locations/" + value.id} artist_url={"artists/" + value.artistId} concert_url={"concerts/" + value.id} venueName={value.venueName} artistGenre={value.artistGenre} searched={this.state.searched} query = {this.state.query} />
                     })}
                   </div>
                   <div className="pagination-menu">
