c7597e51cbcb69c3aece1320a99227c1230ade4c
Abhi Velaga <abhinav.velaga@gmail.com>
2020-04-16 19:58:42 -0500

    chanegd server ips

--- src/components/pages/LocationListPage.js
+++ src/components/pages/LocationListPage.js
@@ -65,7 +65,7 @@ export class LocationListPage extends Component {
   }
 
   updateState() {
-    let url = "http://192.168.1.170:8000/restapi/location";
+    let url = "http://72.182.112.17:8000/restapi/location";
     let options = {
       params: {
         page: this.state.page,
