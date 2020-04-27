b359385e807346cd586f2594a64fef5a8d7dd570
Jason Moy <jason.moy42@gmail.com>
2020-04-26 20:18:44 -0500

    Setup code for visualizations

---
+++ src/components/pages/Visualizations.js
@@ -0,0 +1,16 @@
+import React, { Component, useState } from "react";
+import 
+import numArtistsPerGenre from '../extras/genreData.json';
+import numConcertsPerState from '../extras/stateData.json';
+import avgTicketPrice from '../extras/ticketData.json';
+import BarChart from '../extras/BarChart';
+import BubbleChart from '../extras/BubbleChart';
+import StateChart from '../extras/StateChart';
+
+function Visualizations(props){
+    return (
+    <div>
+
+    </div>
+    );
+}
